15fc702ef1cf9264c4de19a23cadaccb9c300dd5
Elektronikerv <elektronikerv@gmail.com>
2018-06-03 11:39:49 +0300

    Create basis project structure

---
+++ src/main/java/com/inst/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.inst.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.inst")
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        return  viewResolver;
+    }
+}
