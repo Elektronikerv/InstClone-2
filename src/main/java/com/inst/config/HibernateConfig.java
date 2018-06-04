016dd6e41270d0f485b3c53fd878c13b579508fc
Elektronikerv <elektronikerv@gmail.com>
2018-06-05 00:16:46 +0300

    Add hibernate and datasource config

---
+++ src/main/java/com/inst/config/HibernateConfig.java
@@ -0,0 +1,61 @@
+package com.inst.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUsername("kylgovrtpsqxzs");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/inst_db");
+        dataSource.setPassword("b23506a7f2dc2e34a2bf00b5cd94d6efc2ebaf8706f7ee63d39e7ccd25238fee");
+        return  dataSource;
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() throws IOException {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setPackagesToScan("com.inst");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.afterPropertiesSet();
+
+        return  sessionFactory.getObject();
+    }
+
+
+    @Bean
+    public HibernateTransactionManager transactionManager() throws IOException {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory());
+
+        return  transactionManager;
+    }
+
+    private static Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL92Dialect");
+        hibernateProperties.put("hibernate.show_sql", "true");
+
+        return hibernateProperties;
+    }
+
+
+
+}
