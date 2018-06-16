f23fcc91d1b9e07793394a90d2648d6c5a99dc31
Elektronikerv <elektronikerv@gmail.com>
2018-06-16 17:57:19 +0300

    fix bug

--- src/main/java/com/inst/config/WebConfig.java
+++ src/main/java/com/inst/config/WebConfig.java
@@ -35,6 +35,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+        registry.setOrder(1);
     }
 
 }
