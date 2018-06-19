0737ecb0cc15244583c41932c80b6b8b8d21b059
Elektronikerv <elektronikerv@gmail.com>
2018-06-19 13:23:04 +0300

    Add user_subscriptions table with keys

--- src/main/resources/database/tables.sql
+++ src/main/resources/database/tables.sql
@@ -2,21 +2,39 @@
 CREATE TABLE users
 (
     id SERIAL PRIMARY KEY NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    login VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     gender VARCHAR(1) NOT NULL
 );
 ALTER TABLE users
- ADD CONSTRAINT unique_email UNIQUE (email);
+ADD CONSTRAINT unique_login
+UNIQUE (login);
 
 CREATE TABLE images
 (
   id SERIAL PRIMARY KEY NOT NULL,
   content OID NOT NULL,
-  user_id INT NOT NULL
+  user_id INT NOT NULL,
+  created_on TIMESTAMP NOT NULL
 );
 
 ALTER TABLE images
 ADD CONSTRAINT fk_user
 FOREIGN KEY (user_id)
 REFERENCES users(id);
+
+CREATE TABLE user_subscriptions (
+  follower_id INT NOT NULL,
+  following_id INT NOT NULL,
+  PRIMARY KEY (follower_id, following_id)
+);
+
+ALTER TABLE user_subscriptions
+ADD FOREIGN KEY (follower_id)
+REFERENCES users;
+
+ALTER TABLE user_subscriptions
+ADD FOREIGN KEY (following_id)
+REFERENCES users;
