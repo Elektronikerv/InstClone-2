4fb079e4c20018babe6775703a6c1e8d90f05e45
Elektronikerv <elektronikerv@gmail.com>
2018-06-26 11:40:38 +0300

    Make any request authenticated

--- src/main/java/com/inst/config/SecurityConfig.java
+++ src/main/java/com/inst/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/").authenticated()
+                .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .authorizeRequests().antMatchers("/registration").permitAll()
                 .and()
