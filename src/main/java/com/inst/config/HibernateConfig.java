70457af59b0d47f637cf7fd89805f38dc3f90fac
Elektronikerv <elektronikerv@gmail.com>
2018-07-25 12:55:18 +0300

    Move database config data to properties file

--- src/main/java/com/inst/config/HibernateConfig.java
+++ src/main/java/com/inst/config/HibernateConfig.java
@@ -1,8 +1,10 @@
 package com.inst.config;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -12,18 +14,38 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.Properties;
 
+
+
 @Configuration
+@PropertySource("classpath:database.properties")
 @EnableTransactionManagement
 public class HibernateConfig {
+
+    @Value("${database.driverClassName}")
+    private String driverClassName;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.url}")
+    private String url;
+
+    @Value("${database.password}")
+    private String password;
+
+    @Value("${database.hibernate.dialect}")
+    private String dialect;
+
+    @Value("${database.hibernate.show_sql}")
+    private String showSql;
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUsername("kylgovrtpsqxzs");
-        dataSource.setUrl("jdbc:postgresql://ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/dahrnhfndrqi82");
-        dataSource.setPassword("b23506a7f2dc2e34a2bf00b5cd94d6efc2ebaf8706f7ee63d39e7ccd25238fee");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setUrl(url);
+        dataSource.setPassword(password);
         return  dataSource;
     }
 
@@ -46,10 +68,10 @@ public class HibernateConfig {
         return  transactionManager;
     }
 
-    private static Properties hibernateProperties() {
+    private Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL92Dialect");
-        hibernateProperties.put("hibernate.show_sql", "true");
+        hibernateProperties.put("hibernate.dialect", dialect);
+        hibernateProperties.put("hibernate.show_sql", showSql);
 
         return hibernateProperties;
     }
