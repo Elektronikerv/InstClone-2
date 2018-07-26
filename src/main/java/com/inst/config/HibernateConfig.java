e0b0683c3175f7bca3b16a64b102d7bd838a11e1
Elektronikerv <elektronikerv@gmail.com>
2018-07-26 15:11:05 +0300

    fix bug

--- src/main/java/com/inst/config/HibernateConfig.java
+++ src/main/java/com/inst/config/HibernateConfig.java
@@ -42,10 +42,10 @@ public class HibernateConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUsername(username);
-        dataSource.setUrl(url);
-        dataSource.setPassword(password);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUsername("kylgovrtpsqxzs");
+        dataSource.setUrl("jdbc:postgresql://ec2-79-125-110-209.eu-west-1.compute.amazonaws.com:5432/dahrnhfndrqi82");
+        dataSource.setPassword("b23506a7f2dc2e34a2bf00b5cd94d6efc2ebaf8706f7ee63d39e7ccd25238fee");
         return  dataSource;
     }
 
